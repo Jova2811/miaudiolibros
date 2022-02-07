b484811cc7b9419b5d1dc84921a8a209be3ceb82
Hugo Lucio <hugo2811.hl@gmail.com>
2022-02-07 13:35:54 -0600

    Se agrega el proyecto

---
+++ app/src/main/java/net/ivanvega/audiolibros2020/MainActivity.java
@@ -0,0 +1,87 @@
+package net.ivanvega.audiolibros2020;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SelectorFragment selectorFragment
+                = new SelectorFragment();
+
+        if ( findViewById(R.id.contenedor_pequeno) != null    &&
+                getSupportFragmentManager().findFragmentById(R.id.contenedor_pequeno) == null
+        ){
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contenedor_pequeno,
+                            selectorFragment).commit();
+        }
+        //AQUI REVISO SI LE MANDARON PARAMETROS A ESTA ACTIVIDAD
+        //EN ESPECIAL REVISO SI LA ACTIVIDAD LA INVOCO LA NOTIFICACION LANZADA POR EL SERVICIO DE PRIMER PLANO
+        //
+        if(getIntent().getExtras()!=null){
+            Log.d("MSPPN", getIntent().getExtras().getString("rep","no se encontro parametro"));
+            Log.d("MSPPN", getIntent().getExtras().getString("id","no se encontro parametro"));
+            int id = getIntent().getExtras().getInt("id",-1);
+            mostrarDetalle(id, true);
+        }
+
+
+    }
+    DetalleFragment detalleFragment;
+
+    public void mostrarDetalle(int index, boolean comesFromNotification){
+        if(!comesFromNotification) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            if(fragmentManager.findFragmentById(R.id.detalle_fragment) != null){
+                DetalleFragment fragment_detalle = (DetalleFragment) fragmentManager.findFragmentById(R.id.detalle_fragment);
+                fragment_detalle.ponInfoLibro(index);
+            } else {
+                detalleFragment = new DetalleFragment();
+                Bundle bundle = new Bundle();
+                bundle.putInt(DetalleFragment.ARG_ID_LIBRO, index);
+                detalleFragment.setArguments(bundle);
+                fragmentManager.beginTransaction().replace(R.id.contenedor_pequeno, detalleFragment).addToBackStack(null).commit();
+            }
+        }else{
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            detalleFragment = new DetalleFragment();
+            Bundle bundle = new Bundle();
+            bundle.putInt(DetalleFragment.ARG_ID_LIBRO, index);
+            bundle.putBoolean("Service", true);
+            detalleFragment.setArguments(bundle);
+            fragmentManager.beginTransaction().replace(R.id.contenedor_pequeno, detalleFragment).addToBackStack(null).commit();
+        }
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        try {
+            detalleFragment.stopService();
+        }catch (Exception ex){
+
+        }
+    }
+}
---
+++ app/src/main/java/net/ivanvega/audiolibros2020/services/MiServicio.java
@@ -0,0 +1,144 @@
+package net.ivanvega.audiolibros2020.services;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Binder;
+import android.os.Build;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+
+import net.ivanvega.audiolibros2020.MainActivity;
+import net.ivanvega.audiolibros2020.R;
+
+import java.util.Random;
+
+public class MiServicio extends Service {
+
+    private int id;
+    Uri uri;
+    public static MediaPlayer mediaPlayer;
+
+    private final IBinder binder = new MiServicioBinder();
+
+    private final Random mGenerator = new Random();
+
+    /**
+     * Class used for the client Binder.  Because we know this service always
+     * runs in the same process as its clients, we don't need to deal with IPC.
+     */
+    public class MiServicioBinder extends Binder {
+
+        public MiServicio getService() {
+
+            return MiServicio.this;
+        }
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return binder;
+    }
+
+    /** method for clients */
+    public int getRandomNumber() {
+        return mGenerator.nextInt(100);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d("MSAL", "servicio creado");
+
+    }
+
+    private String CHANNEL_ID="CANALID";
+    private void createNotificationChannel() {
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    public MediaPlayer getMediaPlayer(){
+        return mediaPlayer;
+    }
+    String title;
+    String autor;
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        //Este método se manda llamar cuando invocas el servicio con startService()
+        //tarea pesado debe ir en un subproceso y desencadenarse asquí
+        id = intent.getIntExtra("id",-1);
+        title = intent.getStringExtra("title");
+        autor = intent.getStringExtra("autor");
+        //startForeground(10001, new Notication.builder()   );
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createNotificationChannel();
+            foregroundService();
+        }
+        uri = Uri.parse(intent.getStringExtra("uri"));
+        mediaPlayer = MediaPlayer.create(getApplicationContext(),uri);
+
+        Log.d("MSAL", "Iniciando la tarea pesada ");
+        mediaPlayer.start();
+
+        Log.d("MSAL", "Tarea pesada finalizada");
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void foregroundService() {
+
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        notificationIntent.putExtra("id", id);
+        notificationIntent.putExtra("rep", "Servicio Primer plano");
+
+
+        PendingIntent pendingIntent =
+                PendingIntent.getActivity(this, 1002, notificationIntent, 0);
+
+        Notification notification =
+                new Notification.Builder(this, CHANNEL_ID)
+                        .setContentTitle(title)
+                        .setContentText(autor)
+                        .setSmallIcon(R.drawable.ic_launcher_background)
+                        .setContentIntent(pendingIntent)
+                        .setTicker("Se inicio el servicio")
+                        .build();
+
+            // Notification ID cannot be 0.
+             startForeground(1000, notification);
+
+    }
+
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d("MSAL", "Servicio destruido");
+    }
+}
